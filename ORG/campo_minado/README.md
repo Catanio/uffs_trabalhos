f42ccc8e1b3a3506d54fec1867af6e13a57a81bd
Mauricio Catanio <mauricatanio@yahoo.com.br>
2020-07-15 16:47:31 -0300

    identation and README improvements, english translation

--- ORG/campo_minado/README.md
+++ ORG/campo_minado/README.md
@@ -13,23 +13,19 @@ Por especificação do trabalho, deve conter as seguintes funções:
 >* Os registos $v0 e $v1 são utilizados para passar resultados para o procedimento que chamou o procedimento;
 >* O registo $sp é utilizado como apontador para o topo da pilha;
 >* Os registos $s0 a $s7 são preservados por um procedimento;
->* Os registos $t0 a $t9 não necessitam de ser preservados por um procedimento.<br />
-> (Arquitectura de Computadores, pg 36 - Leiria e Moura)
+>* Os registos $t0 a $t9 não necessitam de ser preservados por um procedimento.
+> <p align="right">(Arquitectura de Computadores, pg 36 - Leiria e Moura)</p>
 * Antes de iniciar uma função os registradores utilizados (que não os temporários) são empilhados e apontados pelo $sp.
 Ao final do da função, são desempilhados aos seus valores originais
 
 
-### Explicação as funções
+#### get_adress:
+    Calcula o endereço de uma matriz dado o índice
+    a0: índice da matriz
+    a1: ordem da matriz
+    a2: valor em x
+    a3: valor y
+    v0: retorna o endereço na posição desejada ( -1 se for um endereço inválido)
+    v1: flag de erro; se endereço invalido retorna 1
 
-#### calc_endereço:
-Calcula o endereço de uma matriz dado o índice e <br />
-```
-a0: índice da matriz
-a1: número de linhas
-a2: valor em x
-a3: valor y
-v0: retorna o endereço na posição desejada ( 0 se for um endereço inválido)
-v1: retorna 1 como flag se o endereço for inválido
-
-Operação: ((x*y)+x)* 4bytes + endereço original
-```
+    Operação: ```((x*y) + x) * 4Bytes + endereço original```
--- ORG/campo_minado/campo_minado.asm
+++ ORG/campo_minado/campo_minado.asm
@@ -1,23 +1,39 @@
-###################################################################
-#	func calcula endereço matriz																		#
-#	recebe *campo[], número de linhas, x e y - respectivamente			#
-# retorna endereço e flag de validade de endereço									#
-###################################################################
+#################################
+##	calculate the adress of a certain position inside a square matrix
+#
+#	@param	$a0 = *matrix, $a1 = matrix order, $a2 = x; $a3 = y
+#	@return	$v0 = adress; $v1 = flag;
+get_element_adress:
+	blt	$a2, 0, seg_fault		# if the given position is outside the matrix range {
+	bge	$a2, $a1, seg_fault		#
+	blt	$a3, 0, seg_fault		#
+	bge	$a3, $a1, seg_fault		# }
 
-calc_endereco:
-	blt	$a2, 0, seg_fault
-	bge	$a2, $a1, seg_fault
-	blt	$a3, 0, seg_fault
-	bge	$a3, $a1, seg_fault
+	mul	$t1, $a2, $a3 		# t1 = ((x*y) + x) * 4Bytes
+	add	$t1, $t1, $a2		#
+	mul	$t1, $t1, 4 		# 
+	add	$v0, $t1, $a0		# return = (t1 + original adress)
+	li	$v1, 0				# error flag = 0
+	jr	$ra					# return; 
+	seg_fault:			# returns 1 (flag) to $v1 and -1 instead of adress to $v0
+		li	$v0, -1
+		li	$v1, 1
+		jr	$ra
 
-	mul	$t1, $a1, $a3
-	add	$t1, $t1, $a2
-	mul	$v0, $t1, 4
-	add	$v0, $v0, $a0		# adiciona o offset ao endereço
-	li	$v1, 0					# não retorna flag de erro
-	jr	$ra
+#################################
+##	Check if a given position in the matrix has a bomb
+#
+#	@param $a0 $matrix_adress(x, y)
+#	@return $v0 = 1 if (x, y) possition has a bomb
+is_bomb:
+	addi	$sp, $sp, -4
+	sw	$ra, 0($sp)
 	
-seg_fault:
-	li	$v0, 0			# se tentar acessar uma casa não existente na matriz
-	li	$v1, 1			# retorna 1 (flag) em $v1 e 0 no lugar de endereço
+	beq	$a0, -1, end_ib_func # jumps to end if has a invalid adress
+	lw	$v0, 0($v0)
+	seq	$v0, $v0, 9	# if it has a bomb, returns TRUE
+
+	end_ib_func:
+	lw	$ra, 0($sp)
+	addi	$sp, $sp, 4
 	jr	$ra
